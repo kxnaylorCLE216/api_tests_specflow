4787918d4ae0a66459eba7df83bbda4ab2c6481b
KYLE X NAYLOR <naylorkx@gmail.com>
2019-09-26 18:41:53 -0400

    made the first request

--- api_specflow/api_specflow/UnitTest1.cs
+++ api_specflow/api_specflow/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using RestSharp;
 using System;
 
 namespace api_specflow
@@ -9,6 +10,13 @@ namespace api_specflow
         [Test]
         public void TestMethod1()
         {
+            var client = new RestClient("http://localhost:3000/");
+
+            var request = new RestRequest("products/{productid}", Method.GET);
+            request.AddUrlSegment("productid", 1);
+
+            var content = client.Execute(request).Content;
+
         }
     }
 }
