551d199b1ba5dd75c6e6b119658a20f47a1fa882
KYLE X NAYLOR <naylorkx@gmail.com>
2019-09-29 08:51:55 -0400

    SpecFlow test #2

---
+++ api_specflow/api_specflow/Features/PostProduct.feature
@@ -0,0 +1,9 @@
+﻿Feature: PostProduct
+	Test POST operation using Restsharp.net library
+
+@smoke
+Scenario: Verify Post operation for Product
+	Given I Perform POST operation for 'products' with body
+	| name								    | cost		|
+	| Kraft Easy Mac & Cheese Dinner Cups	|	5.98	|
+	Then I should see the 'name' as 'Kraft Easy Mac & Cheese Dinner Cups'
---
+++ api_specflow/api_specflow/Steps/PostProductSteps.cs
@@ -0,0 +1,32 @@
+﻿using api_specflow.Model;
+using api_specflow.Utilities;
+using RestSharp;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace api_specflow
+{
+    [Binding]
+    public class PostProductSteps
+    {
+        private Settings _settings;
+
+        public PostProductSteps(Settings settings) => _settings = settings;
+
+        [Given(@"I Perform POST operation for '(.*)' with body")]
+        public void GivenIPerformPOSTOperationForWithBody(string url, Table table)
+        {
+            dynamic data = table.CreateDynamicInstance();
+
+            _settings.Request = new RestRequest(url, Method.POST);
+
+            _settings.Request.AddJsonBody(new
+            {
+                name = data.name,
+                cost = data.cost
+            });
+
+            _settings.Response = _settings.RestClient.ExecuteAsyncRequest<Products>(_settings.Request).GetAwaiter().GetResult();
+        }
+    }
+}
