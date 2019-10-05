9f9f99e5ddd0f13df08fb948bb2e25b9fb6e26c1
KYLE X NAYLOR <naylorkx@gmail.com>
2019-10-05 16:44:14 -0400

    running tests in parallel

--- api_specflow/api_specflow/Properties/AssemblyInfo.cs
+++ api_specflow/api_specflow/Properties/AssemblyInfo.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
@@ -11,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("98672122-e66a-441a-8aad-a1f2c3b4a767")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: Parallelizable(ParallelScope.Fixtures)]
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
